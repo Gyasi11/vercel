bb9765d1bd02d5447632e16f11d754aef908f185
Gyasi11 <glcalhoun1123@yahoo.com>
2020-11-02 13:57:44 -0500

    Content section

--- pages/index.js
+++ pages/index.js
@@ -18,37 +18,71 @@ export default function Home() {
         
         </div>
         <div className={styles.gridItem}>
-          <div className={styles.circularIcon}></div>
+          <div className={styles.circularIcon}>
+            <img src="/coding.png" />
+          </div>
           <h4>Fullstack and Frontend Developer Experience</h4>
           <p className={styles.copy}>
           <p>
               The Fall of October 2019 I started working as a Digital Frontend Developer for Progressive Insurance. Since working at Progressive I've programmed using langauges such as: JavaScript, HTML5, CSS3 and worked in VueJS.
-            </p>
+          </p>
 
-            <p> I've modernized Progressive Insurances brand and design guidelines site by re-developing the app from Vanilla HTML,CSS and JS to a VueJS application.
+          <p> I've modernized Progressive Insurances brand and design guidelines site by re-developing the app from Vanilla HTML,CSS and JS to a VueJS application.
               While working at Progressive Insurance I've also built new web pages for Progressive.com, display ads, an Android Application new modules for Progressive.com developers 
-               </p>
+         </p>
             After graduating with a degree in Computer science May of 2018 I work as a full stack application developer at Hyland Software.
             During my time at Hyland I programmed with languages such as: C#, JavaScript, TypeScript, and worked in AngularJS and Angular 2+.
-            </p>
-            <p> While at Hyland I helped our Development team re-design, modernize and build new responsive UI's for Hyland Content Connect Angular App.
-          </p>
-            
+        </p>
+        <p> While at Hyland I helped our Development team re-design, modernize and build new responsive UI's for Hyland Content Connect Angular App.
+        </p> 
+        <button>Download resume here</button>
          
-            <button>Download resume here</button>
-         
-        </div>
-        <div className={styles.gridItem}>
-          
-        </div>
-        <div className={styles.gridItem}>
-          
-        </div>
       </div>
-      <div className={styles.youTube}>
+        <div className={styles.gridItem}></div>
+        <div className={styles.gridItem}>
+          <div id={styles.cardDevelopment}>
+            <img src="/blake-connally-B3l0g6HLxr8-unsplash.jpg"/>
+              <h3> My Top Programming Skillsets </h3>
+              <p>My favorite programming Languages and Frameworks are JavaScript, Python, Material Design,  CSS/HTML, Angular, VueJS, and Next.JS. </p>
+          </div>
+        </div>
+        <div className={styles.gridItem}></div>
+      </div>
 
+      <div className={styles.youTube}>
+        <div className={styles.gridItem}></div>
+        <div className={styles.gridItem}>
+        <div id={styles.contentImage}>
+          <figure >
+            <img src="/contentCreation.jpg"/>
+          </figure>
+         
+          </div>
+        </div>
+        <div className={styles.gridItem}></div>
+        <div className={styles.gridItem}>
+        <div className={styles.circularIcon}>
+          <figure>
+            <img src="/camera.png" />
+          </figure>
+            
+          </div>
+          <h4>YoutTube Content Creator</h4>
+      
+         <p>
+           Over the past few years on YouTube I've had the wonderful opportunity gain over 60K subscribers along with over 4 million channels views.
+           Through this experience I've found a new passion of mine which is digital content creation.
+          </p>
+          <p>
+            One to two times a week I make videos on YouTube centered around education (software engineering), consumer tech, and lifestyle.
+         </p>
+         <p>
+           I've also enjoyed product photography and showing my process of creating the semi-perfect desk setup as a content creator and youtuber.
+         </p>
+        <button>Checkout my YouTube Channel!</button>
+        </div>
+        <div className={styles.gridItem}></div>
       </div>
-  
     </div>
 
   )
